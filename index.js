0a58607ebaf8ccb6b67574ac75b2c0c20c58606b
bluepadu <padum3955@gmail.com>
2021-06-25 15:56:53 +0000

    modify

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ const makeCommit = (n) => {
   if (n === 0) return simpleGit().push();
   const x = random.int(0, 54);
   const y = random.int(0, 6);
-  const DATE = moment()
-    .subtract(10, "y")
+  var DATE = moment()
+    .subtract(9, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
     .format();
-  const data = {
+  var data = {
     date: DATE,
   };
   console.log(DATE);
+  if(DATE.substring(0,7)== "2021-07"){
+    DATE = DATE.replace("7", "6");
+    console.log("this is replace committed into " + DATE);
+  }
   jsonfile.writeFile(FILE_PATH, data, () => {
     simpleGit()
       .add([FILE_PATH])
@@ -28,4 +32,4 @@ const makeCommit = (n) => {
   });
 };
 
-makeCommit(120);
+makeCommit(520);
